3545bfd921c49572b194e8a7cdd174460e63aeaa
glochen-pulley <gloria@withpulley.com>
2025-10-10 10:38:59 -0700

    stamp

--- src/home/HomeHeader.tsx
+++ src/home/HomeHeader.tsx
@@ -1,12 +1,18 @@
-import { ChineseH2, H2 } from "../design/Typography";
+import { ChineseH1, ChineseH2, H1, H2 } from "../design/Typography";
 
 export function HomeHeader() {
   return (
-    <div className="flex flex-row gap-4 items-center justify-center w-full py-8">
-      <ChineseH2>
-        哈咯 ヾ{"("}^-^{")"}
-      </ChineseH2>
-      <H2>hope you have a fun romp around</H2>
-    </div>
+    <>
+      <div className="flex flex-row py-20 px-8 justify-center gap-6">
+        <img src="/chinese_seal.png" alt="Chinese seal" className="h-20" />
+        <div className="flex flex-col justify-left gap-6">
+          <ChineseH1>
+            哈咯 ヾ{"("}^-^{")"}
+          </ChineseH1>
+          <H2>hope you have a fun romp around</H2>
+        </div>
+      </div>
+      <div className="brush-stroke-line"></div>
+    </>
   );
 }
